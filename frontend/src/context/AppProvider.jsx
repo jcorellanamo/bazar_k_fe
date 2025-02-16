1ddffdcf068b88438d86455843834a88894f3c29
Roger <rogerlinarez@hotmail.com>
2025-02-16 13:21:12 -0300

    Se agrega vista Carrito

--- frontend/src/context/AppProvider.jsx
+++ frontend/src/context/AppProvider.jsx
@@ -1,13 +1,15 @@
 // src/context/AppProvider.jsx
 import { createContext, useState, useEffect } from "react";
+import Swal from "sweetalert2";
 
 export const kfeContext = createContext();
 
 const AppProvider = ({ children }) => {
-  const [producto, setProducto] = useState([]); // Arreglo que almacenará los productos
-  const [carrito, setCarrito] = useState([]); // Arreglo para el carrito de compras
+  const [producto, setProducto] = useState([]);
+  const [carrito, setCarrito] = useState([]);
+  const [totalAPagar, setTotalAPagar] = useState(0);
+  const [actualizarTotal, setActualizarTotal] = useState(false);
 
-  // Cargar los productos desde el archivo JSON al montar el componente
   useEffect(() => {
     getProductos();
   }, []);
@@ -15,22 +17,102 @@ const AppProvider = ({ children }) => {
   const getProductos = async () => {
     try {
       const res = await fetch("/bazarKFe_Productos.json");
-      if (!res.ok) {
-        throw new Error("No se pudo obtener los productos");
-      }
+      if (!res.ok) throw new Error("No se pudo obtener los productos");
       const productos = await res.json();
-      setProducto(productos); // Actualiza el estado con los productos
+      setProducto(productos);
     } catch (error) {
       console.error("Error al cargar los productos:", error);
     }
   };
 
+  const agregarCarrito = (producto, cantidad) => {
+    const productoExistente = carrito.find(
+      (prod) => prod.id_producto === producto.id_producto
+    );
+
+    if (productoExistente) {
+      // Si el producto ya está en el carrito, solo sumamos 1 a la cantidad.
+      const updatedCarrito = carrito.map((prod) =>
+        prod.id_producto === producto.id_producto
+          ? { ...prod, count: prod.count + cantidad }
+          : prod
+      );
+      setCarrito(updatedCarrito);
+    } else {
+      // Si no está en el carrito, lo agregamos con la cantidad inicial
+      const nuevoProducto = { ...producto, count: cantidad };
+      setCarrito([...carrito, nuevoProducto]);
+    }
+
+    setActualizarTotal(true);
+  };
+
+  const eliminarCarrito = (producto) => {
+    const productoExistenteIndex = carrito.findIndex(
+      (prod) => prod.id_producto === producto.id_producto
+    );
+
+    if (productoExistenteIndex >= 0) {
+      if (carrito[productoExistenteIndex].count > 1) {
+        const updatedCarrito = carrito.map((prod) =>
+          prod.id_producto === producto.id_producto
+            ? { ...prod, count: prod.count - 1 }
+            : prod
+        );
+        setCarrito(updatedCarrito);
+      } else {
+        Swal.fire({
+          title: "¿Estás seguro de eliminar tu producto?",
+          text: "¡No podrás revertir esto!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "¡Sí, eliminar!",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            const updatedCarrito = [...carrito];
+            updatedCarrito.splice(productoExistenteIndex, 1);
+            setCarrito(updatedCarrito);
+            Swal.fire({
+              title: "¡Eliminada!",
+              text: "Tu producto fue eliminada.",
+              icon: "success",
+            });
+            setActualizarTotal(true);
+          }
+        });
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (actualizarTotal) {
+      const total = carrito.reduce((acc, producto) => {
+        return (
+          acc +
+          parseFloat(producto.precio.replace("$", "").replace(".", "")) *
+            producto.count
+        );
+      }, 0);
+      setTotalAPagar(total);
+      setActualizarTotal(false);
+    }
+  }, [actualizarTotal, carrito]);
+
   return (
-    <kfeContext.Provider value={{ producto, setProducto, carrito, setCarrito }}>
+    <kfeContext.Provider
+      value={{
+        producto,
+        carrito,
+        totalAPagar,
+        agregarCarrito,
+        eliminarCarrito,
+      }}
+    >
       {children}
     </kfeContext.Provider>
   );
 };
 
 export default AppProvider;
-
