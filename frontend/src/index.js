42a2cbbe0313cd86751458c5bf13294cd21df56d
Bernardo Gaete <bernardogaetecancino@gmail.com>
2025-03-05 19:10:45 -0300

    Se crean variables de entorno para el frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,21 @@
 // src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // Mantén el BrowserRouter aquí
+import { BrowserRouter } from "react-router-dom"; 
 import "./index.css";
 import App from "./App";
+import axios from "axios";
+
 
 import AppProvider from "./context/AppProvider";
 
-import ErrorBoundary from "./components/ErrorBoundary"; // Mantén el ErrorBoundary
+import ErrorBoundary from "./components/ErrorBoundary"; 
+
+
+console.log("Base URL:", process.env.REACT_APP_API_URL)
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
