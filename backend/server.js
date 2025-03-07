34eac9a699a737110743c92da7bb2fc08d39315e
Juan Orellana <jcorellanamolina@gmail.com>
2025-03-07 12:06:33 -0300

    confi de server

--- backend/server.js
+++ backend/server.js
@@ -30,10 +30,6 @@ require("dotenv").config();
 const app = express();
 // server.js
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
-
 
 // Middlewares
 app.use(cors());
